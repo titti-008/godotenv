4803c063b20489b4a82a954a7fe0632e90065e4b
titti-008 <ikuzah_m2005@yahoo.co.jp>
2023-06-13 22:36:12 +0900

    fix: fix bug expandVarRegex

--- parser.go
+++ parser.go
@@ -250,8 +250,8 @@ func isLineEnd(r rune) bool {
 
 var (
 	escapeRegex        = regexp.MustCompile(`\\.`)
-	expandVarRegex     = regexp.MustCompile(`(\\)?(\$)(\()?\{([A-Za-z0-9_]+)?\}?`)
-	unescapeCharsRegex = regexp.MustCompile(`\\([^&])`)
+	expandVarRegex     = regexp.MustCompile(`(\\)?(\$)(\()?\{?([A-Z0-9_]+)?\}?`)
+	unescapeCharsRegex = regexp.MustCompile(`\\([^$])`)
 )
 
 func expandVariables(v string, m map[string]string) string {
@@ -261,7 +261,7 @@ func expandVariables(v string, m map[string]string) string {
 		if submatch == nil {
 			return s
 		}
-		if submatch[1] == `\\` || submatch[2] == "(" {
+		if submatch[1] == "\\" || submatch[2] == "(" {
 			return submatch[0][1:]
 		} else if submatch[4] != "" {
 			return m[submatch[4]]
